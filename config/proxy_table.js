a5c2d3c2706b5f6938c3929c9f3159a12786bfb2
life1st <jiaoyangdev@gmail.com>
2021-04-12 23:02:37 +0800

    update: source path.

--- config/proxy_table.js
+++ config/proxy_table.js
@@ -1,7 +1,7 @@
 const table = {
   'life1st.me': 'github.com/life1st',
   'redirect.life1st.me': 'github.com/life1st/redirect',
-  'resume.life1st.me': 'https://blog.life1st.me/resume/resume_201810.html'
+  'resume.life1st.me': 'https://blog.life1st.me/resume/resume_202104.html'
 }
 
 module.exports = {
