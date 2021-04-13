81d8ff7ba80ef1334b13685919154c3c95732932
life1st <jiaoyangdev@gmail.com>
2021-04-13 10:23:48 +0800

    update: pdf path.

--- config/proxy_table.js
+++ config/proxy_table.js
@@ -1,7 +1,8 @@
 const table = {
   'life1st.me': 'github.com/life1st',
   'redirect.life1st.me': 'github.com/life1st/redirect',
-  'resume.life1st.me': 'https://blog.life1st.me/resume/resume_202104.html'
+  'resume.life1st.me': 'https://blog.life1st.me/resume/resume_202104.html',
+  'resume.life1st.me/pdf': 'https://blog.life1st.me/resume/pdf/前端开发工程师_焦洋.pdf',
 }
 
 module.exports = {
