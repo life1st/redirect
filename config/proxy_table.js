ab7e306a485a4e888dd5ccc32e652298625ec4a4
Yang <jiaoyangdev@gmail.com>
2022-07-31 19:31:29 +0800

    Update proxy_table.js

--- config/proxy_table.js
+++ config/proxy_table.js
@@ -3,7 +3,6 @@ const table = {
   'redirect.life1st.me': 'github.com/life1st/redirect',
   'resume.life1st.me': 'https://blog.life1st.me/resume/resume_202104.html',
   'resume.life1st.me/pdf': 'https://blog.life1st.me/resume/pdf/前端开发工程师_焦洋.pdf',
-  'home.life1st.me': 'https://jiaoy.myqnapcloud.com:44443',
 }
 
 module.exports = {
